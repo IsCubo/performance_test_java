02a0f459b1e450d03e109c348579b65637c8ef39
Shnaider.Jordan <shnaiderjordan04@outlook.com>
2025-10-14 14:44:16 -0500

    Main folder structure of the project with layered architecture.

---
+++ src/main/java/com/mycompany/libronova/model/enums/UserRole.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.libronova.model.enums;
+
+/**
+ *
+ * @author Coder
+ */
+public enum UserRole {
+    
+}
